c47f2f8eb8cc481c677da9b5b3b33c703e7545fa
03bethlehem <bethlehemlakew@gmail.com>
2022-04-11 16:52:03 -0700

    Tas 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print lowercase alphabet excluding c and q
+ * Return: 0
+ */
+int mai(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++)
+{
+if (c != 'e' && c != 'q')
+putchar(c);
+}
+
+putchar('\n');
+
+return (0);
+}
