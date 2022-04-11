164b3f1ad3335074ef9182bfba75137d1c9d858a
03bethlehem <bethlehemlakew@gmail.com>
2022-04-11 10:44:27 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio>
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+write(1, "and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
