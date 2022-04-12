a91d1263764f1f8762125d3c535dcdd9e137bf42
03bethlehem <bethlehemlakew@gmail.com>
2022-04-12 05:20:05 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,11 +10,11 @@ int main(void)
 
 	int c;
 
-	for (c = 0: c < 8; c++)
+	for (c = 0; c < 8; c++;)
 	{
-		_putchar(bethlehem[c];
+		_putchar(bethlehem[c]);
 
 	}
-        _putchar('\n')
+        _putchar('\n');
         return (0);	
 }
