2317f1a06f87c055b5cda4e889eba15ee4272408
03bethlehem <bethlehemlakew@gmail.com>
2022-04-11 18:02:03 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: prints all single digit numbers of base 10  
+ * Description: Prints all single digit numbers of base 10  
  * starting from 0, followed by a new line.
  * Return: 0
  */
