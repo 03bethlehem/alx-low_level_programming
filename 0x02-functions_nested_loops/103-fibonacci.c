b231d13b130f58598fd43075f32bcea1107e823b
03bethlehem <bethlehemlakew@gmail.com>
2022-04-13 16:40:45 -0700

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -17,11 +17,11 @@ fibsum = fib1 + fib2;
 if (fibsum > 4000000)
 break;
 
-if ((fibsum % 2) == 0
+if ((fibsum % 2) == 0)
 tot_sum += fibsum;
 
 fib1 = fib2;
-fib2 fibsum;
+fib2 = fibsum;
 }
 printf("%.0f\n", tot_sum);
 
