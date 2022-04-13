93cab068b7cdfab717c327b2099efd7013da88d5
03bethlehem <bethlehemlakew@gmail.com>
2022-04-13 11:59:51 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -24,10 +24,10 @@ void print_to_98(int n)
 	{
 		for (i = n; i < 99; i++)
 		{
-			        print("%d", i);
+			        printf("%d", i);
 			        if (i != 98)
 				{
-					prinf(", ");
+					printf(", ");
 				}
 		}
 	}
