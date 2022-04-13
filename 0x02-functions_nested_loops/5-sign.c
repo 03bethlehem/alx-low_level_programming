6bb4e59585962e8f2d7891fca0db45d6a443ff75
03bethlehem <bethlehemlakew@gmail.com>
2022-04-13 03:04:31 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,14 +12,16 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-	_putchar('+');
-	return (1);
-	else if (n == 0)
-	{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
 _putchar('0');
-	}
-	else
-	{_putchar('_');
-		return (-1);
-	}
+}
+else
+{
+_putchar('_');
+return (-1);
+}
 }
