4ca5f4255ed9968f295b6aa64fc5a8141b761ae2
03bethlehem <bethlehemlakew@gmail.com>
2022-04-14 12:36:31 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * print_numbers - print numbers
+ * print_numbers - print the number zero to nine
+ *
  * Return: Always 0.
  */
 void print_numbers(void)
@@ -8,11 +9,10 @@ void print_numbers(void)
 int i;
 
 i = 0;
-while (i < 10)
-{
+
+for (i = 0; i < 10; i++)
+
 _putchar (i + '0');
-i++;
-}
 
 _putchar ('\n');
 }
