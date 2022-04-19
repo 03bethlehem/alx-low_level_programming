f616581f50cc039cefc0c75fb47d821ece0d4580
03bethlehem <bethlehemlakew@gmail.com>
2022-04-18 19:45:54 -0700

    101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+include <stdlib.h>
+include <time.h>
+include <studio.h>
+/**
+ * my task
+ */
+int main(void)
+{
+int num;
+srand(time(0));
+num = rand();
+printf("%i\n", num);
+return (0);
+}
