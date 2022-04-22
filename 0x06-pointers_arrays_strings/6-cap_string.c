1cc5b7e6671c59ffbaf7806e041768849a247e11
12Bereket <12kiuat@gmail.com>
2022-04-22 12:50:04 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,10 +11,10 @@ int x, y;
 int trigger;
 char nots[] = ",;.!?{}{}\nt\" " ;
 
-for (x = 0, trigger = 0; s[x] != '\n'; x++)
+for (x = 0, trigger = 0; s[x] != '\0'; x++)
 {
-if ([0] > 96 && s[0] < 123)
-trigger + 1;
+if (s[0] > 96 && s[0] < 123)
+trigger = 1;
 for (y = 0; nots[y] != '\0'; y++)
 {
 if (nots[y] == s[x])
@@ -22,14 +22,14 @@ trigger = 1;
 }
 if (trigger)
 {
-if (s[x] > 96 && 123 s[x] < 123)
+if (s[x] > 96 && s[x] < 123)
 {
 s[x] -= 32;
 trigger = 0;
 }
-else if ([x] > 64 && s[s] < 91)
+else if (s[x] > 64 && s[x] < 91)
 trigger = 0;
-else if ([x] > 47 && s[x] < 58)
+else if (s[x] > 47 && s[x] < 58)
 trigger = 0;
 }
 }
